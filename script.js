83b0490906a81ba2b0f99feba26d64e9bd79a039
yzkee <yzkee@scholastic.asia>
2016-06-26 20:15:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,37 +7,18 @@ const scriptRules = require('./script.json');
 
 module.exports = new Script({
     processing: {
-        prompt: (bot) => bot.say('Beep boop...'),
+        //prompt: (bot) => bot.say('Beep boop...'),
         receive: () => 'processing'
     },
 
     start: {
         receive: (bot) => {
-            return bot.say('Hi! I\'m YZkee Bot!')
-                .then(() => 'askName');
+            return bot.say('Hi ! Just say HELLO to get started.')
+                .then(() => 'speak');
         }
     },
 
-    askName: {
-        prompt: (bot) => bot.say('What\'s your name?'),
-        receive: (bot, message) => {
-            const name = message.text;
-            return bot.setProp('name', name)
-                .then(() => bot.say(`Great! I'll call you ${name}
-Is that OK? %[Yes](postback:yes) %[No](postback:no)`))
-                .then(() => 'finish');
-        }
-    },
-
-    finish: {
-        receive: (bot, message) => {
-            return bot.getProp('name')
-                .then((name) => bot.say(`Sorry ${name}, my creator didn't ` +
-                        'teach me how to do anything else!'))
-                .then(() => 'finish');
-        }
-    }
- speak: {
+    speak: {
         receive: (bot, message) => {
 
             let upperText = message.text.trim().toUpperCase();
